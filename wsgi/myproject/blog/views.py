84b75cfae88dfcdb4c42a0cb45556e9a006aeb29
Pedro Miguel <pedro.miguel@live.co.uk>
2015-12-21 20:36:18 +0000

    Created index view to display all published posts.

--- wsgi/myproject/blog/views.py
+++ wsgi/myproject/blog/views.py
@@ -1,5 +1,15 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Posts
 
 
 def index(request):
-    return HttpResponse("Hello, world!!")
+    """
+    Returns all posts ordered by date of creation (new post first)
+
+    :param request:  a get request
+    """
+    posts = Posts.objects.order_by('-date_created')
+    context = {'posts': posts}
+
+    return render(request, 'blog/index.html', context)
