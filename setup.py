4011b36aac4917676010af7647067b418b8aa061
Pedro Miguel <pedro.miguel@live.co.uk>
2015-12-21 22:29:20 +0000

    Updated project dependencies

--- setup.py
+++ setup.py
@@ -17,9 +17,13 @@ setup(
     url='http://www.python.org/sigs/distutils-sig/',
     # GETTING-STARTED: define required django version:
     install_requires=[
-        'Django==1.8.4'
+        'Django==1.9',
+        'psycopg2==2.6.1',
+        'markdown==2.6.5',
     ],
     dependency_links=[
-        'https://pypi.python.org/simple/django/'
+        'https://pypi.python.org/simple/django/',
+        'https://pypi.python.org/simple/psycopg2/',
+        'https://pypi.python.org/simple/markdown/',
     ],
 )
