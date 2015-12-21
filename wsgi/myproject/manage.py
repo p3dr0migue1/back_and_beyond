3124a9a4479a02d40bece96df8123ca400837544
Pedro Miguel <pedro.miguel@live.co.uk>
2015-12-21 21:24:01 +0000

    Created development virtualenv hook to use development settings.

--- wsgi/myproject/manage.py
+++ wsgi/myproject/manage.py
@@ -3,10 +3,13 @@ import os
 import sys
 
 if __name__ == "__main__":
-    # GETTING-STARTED: change 'myproject' to your project name:
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "myproject.settings.production")
+    if 'DEVELOPMENT_SETTINGS' in os.environ.keys():
+        settings_file = os.environ.get('DEVELOPMENT_SETTINGS')
+    else:
+        settings_file = "myproject.settings.production"
 
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", settings_file)
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
