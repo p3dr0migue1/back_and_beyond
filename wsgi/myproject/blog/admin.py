a3d0a7ea09523609f95ea28ae91a804fff57196a
Pedro Miguel <pedro.miguel@live.co.uk>
2015-12-21 10:44:14 +0000

    Added admin configuration, added blog app index template, added project base template and project css file.

--- wsgi/myproject/blog/admin.py
+++ wsgi/myproject/blog/admin.py
@@ -1,3 +1,29 @@
-from django.contrib import admin
+import models
 
-# Register your models here.
+from django.contrib import admin
+from .models import Tag, Posts, PostTags
+
+
+class TagsAdmin(admin.ModelAdmin):
+    # Prepopulates the slug field automatically
+    # from the title field
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('name', 'slug')
+
+
+class PostTagsInline(admin.TabularInline):
+    model = models.PostTags
+    extra = 1
+
+
+class PostsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    # readonly_fields = ('date_created', 'last_updated')
+    inlines = [PostTagsInline]
+
+    list_display = ('title', 'slug', 'get_tag_names')
+    # exclude = ('html_content')
+
+
+admin.site.register(Tag, TagsAdmin)
+admin.site.register(Posts, PostsAdmin)
