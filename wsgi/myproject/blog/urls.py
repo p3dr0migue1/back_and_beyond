cbb95f68e30db0e44b96cbed18ac9a0883e4337f
Pedro Miguel <pedro.miguel@live.co.uk>
2016-02-01 22:15:30 +0000

    Added login and logout urls.

--- wsgi/myproject/blog/urls.py
+++ wsgi/myproject/blog/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -6,5 +7,11 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<tag_slug>[\w\-]+)$', views.posts_in_tag, name='posts_in_tag')
+    url(
+        r'^tags/(?P<tag_slug>[\w\-]+)/$',
+        views.posts_in_tag,
+        name='posts_in_tag'
+    ),
+    url(r'^login/$', views.custom_login, name='login'),
+    url(r'^logout/$', auth_views.logout_then_login, name='logout')
 ]
