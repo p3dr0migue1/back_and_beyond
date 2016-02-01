3621b8aa699858f3e52aa09658e4c6df4c5a94da
Pedro Miguel <pedro.miguel@live.co.uk>
2016-02-01 22:16:59 +0000

    Added login redirect variables in global settings.

--- wsgi/myproject/myproject/settings/base.py
+++ wsgi/myproject/myproject/settings/base.py
@@ -87,6 +87,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
