f3a692c024037238f4ea0cff139daed384ebcd06
Pedro Miguel <pedro.miguel@live.co.uk>
2016-02-01 22:18:26 +0000

    Overrided django login function, added login required decorator to all views. Removed un-necessary comment.

--- wsgi/myproject/blog/views.py
+++ wsgi/myproject/blog/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import login as django_login
+from django.conf import settings
 
 from .models import Posts, PostTags
 
@@ -7,6 +10,13 @@ def get_associated_tags():
     return PostTags.get_tags_associated_with_posts()
 
 
+def custom_login(request):
+    if request.user.is_authenticated():
+        return redirect(settings.LOGIN_REDIRECT_URL)
+    return django_login(request)
+
+
+@login_required
 def index(request):
     """
     Returns all posts ordered by date of creation (new post first)
@@ -19,6 +29,7 @@ def index(request):
     return render(request, 'blog/index.html', context)
 
 
+@login_required
 def posts_in_tag(request, tag_slug):
     posts = Posts.get_posts_in_tag(tag_slug)
     context = {'tags': get_associated_tags(), 'posts': posts}
