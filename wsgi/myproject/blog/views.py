175a198cec6344fb4ecde42a94d2cc3186991465
Pedro Miguel <pedro.miguel@live.co.uk>
2016-03-31 00:03:38 +0100

    WIP pagination.

--- wsgi/myproject/blog/views.py
+++ wsgi/myproject/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import login as django_login
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.conf import settings
+
 
 from .models import Posts, PostTags
 
@@ -16,6 +18,23 @@ def custom_login(request):
     return django_login(request)
 
 
+def pagination(request, object_list):
+    # show 5 posts per page
+    paginator = Paginator(object_list, 5)
+    page = request.GET.get('page')
+
+    try:
+        pages = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer, deliver first page.
+        pages = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range (e.g. 9999), deliver
+        # last page of results.
+        pages = paginator.page(paginator.num_pages)
+    return pages
+
+
 @login_required
 def index(request):
     """
@@ -23,7 +42,10 @@ def index(request):
 
     :param request:  a get request
     """
-    posts = Posts.objects.order_by('-date_created').filter(status=2)
+    post_list = Posts.objects.order_by('-date_created').filter(status=2)
+    posts = pagination(request, post_list)
+
+    import ipdb; ipdb.set_trace()
     context = {'tags': get_associated_tags(), 'posts': posts}
 
     return render(request, 'blog/index.html', context)
@@ -31,7 +53,9 @@ def index(request):
 
 @login_required
 def posts_in_tag(request, tag_slug):
-    posts = Posts.get_posts_in_tag(tag_slug)
+    post_list = Posts.get_posts_in_tag(tag_slug)
+    posts = Base()
+    posts.pagination(post_list, 5, request)
     context = {'tags': get_associated_tags(), 'posts': posts}
 
     return render(request, 'blog/posts_in_tag.html', context)
