6d3ae1d0ad596ff343a0249c447ed177634e26e3
Pedro Miguel <pedro.miguel@live.co.uk>
2016-06-12 14:50:28 +0100

    Adds check to determine which environment to use.

--- wsgi/myproject/myproject/settings/development.py
+++ wsgi/myproject/myproject/settings/development.py
@@ -6,9 +6,6 @@ from .base import *
 
 DEBUG = True
 
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
--- wsgi/myproject/myproject/wsgi.py
+++ wsgi/myproject/myproject/wsgi.py
@@ -11,8 +11,13 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+if 'DEVELOPMENT_SETTINGS' in os.environ.keys():
+    settings_file = os.environ.get('DEVELOPMENT_SETTINGS')
+else:
+    settings_file = "myproject.settings.production"
+
 # GETTING-STARTED: change 'myproject' to your project name:
 os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "myproject.settings.production")
+    "DJANGO_SETTINGS_MODULE", settings_file)
 
 application = get_wsgi_application()
