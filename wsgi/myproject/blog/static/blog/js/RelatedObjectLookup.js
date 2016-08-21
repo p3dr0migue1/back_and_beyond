c47afbe10096300f9d4d5b83641ab2b7048f236d
Pedro Miguel <pedro.miguel@live.co.uk>
2016-08-21 17:44:58 +0100

    Refactored `dismissAddAnotherPopup` to work with checkboxes.

--- wsgi/myproject/blog/static/blog/js/RelatedObjectLookup.js
+++ wsgi/myproject/blog/static/blog/js/RelatedObjectLookup.js
@@ -41,33 +41,30 @@ function showAddAnotherTagPopup(triggeringLink) {
     return false;
 }
 
-function dismissAddAnotherPopup(win, newId, newRepr) {
-    // newId and newRepr are expected to have previously been escaped by
-    // django.utils.html.escape.
-    debugger;
-    newId = html_unescape(newId);
-    newRepr = html_unescape(newRepr);
-    var name = windowname_to_id(win.name);
-    var elem = document.getElementById(name);
-    if (elem) {
-        var elemName = elem.nodeName.toUpperCase();
-        if (elemName == 'SELECT') {
+(function($) {
+    dismissAddAnotherPopup = function dismissAddAnotherPopup(win, newId, newRepr) {
+        // newId and newRepr are expected to have previously been escaped by
+        // django.utils.html.escape.
+        newId = html_unescape(newId);
+        newRepr = html_unescape(newRepr);
+        var name = windowname_to_id(win.name);
+        var elem = document.getElementById(name);
+        if (elem) {
+            $(
+                '<li>'
+               +'<label for="' + name + '_' +  newId + '">'
+               +'<input id="' + name + '_' + newId + '" name="tags" value="' + newId + '" type="checkbox" checked="checked">' + newRepr
+               +'</input>'
+               +'</label>'
+               +'</li>'
+             ).appendTo(elem);
+        } else {
+            var toId = name + "_to";
+            elem = document.getElementById(toId);
             var o = new Option(newRepr, newId);
-            elem.options[elem.options.length] = o;
-            o.selected = true;
-        } else if (elemName == 'INPUT') {
-            if (elem.className.indexOf('vManyToManyRawIdAdminField') != -1 && elem.value) {
-                elem.value += ',' + newId;
-            } else {
-                elem.value = newId;
-            }
+            // SelectBox.add_to_cache(toId, o);
+            // SelectBox.redisplay(toId);
         }
-    } else {
-        var toId = name + "_to";
-        elem = document.getElementById(toId);
-        var o = new Option(newRepr, newId);
-        // SelectBox.add_to_cache(toId, o);
-        // SelectBox.redisplay(toId);
+        win.close();
     }
-    win.close();
-}
+})(jQuery);
