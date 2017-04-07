8b91380e1fbc4a938421cc388a4009d12676f51f
Pedro Curado <pedro.miguel@live.co.uk>
2017-04-07 17:19:20 +0100

    Adds test for the index page. Adds test to check user is redirected to the login page if not logged in.

--- wsgi/back_and_beyond/blog/tests.py
+++ wsgi/back_and_beyond/blog/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import AnonymousUser, User
 
-# Create your tests here.
+from .views import index
+
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_superuser(
+            'pedro',
+            'pedro@mail.co.uk',
+            'admin'
+        )
+
+    def test_access_index_view_with_signout_user(self):
+        request = self.factory.get('/')
+        request.user = AnonymousUser()
+        response = index(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/login/?next=/')
+
+    def test_index_view(self):
+        request = self.factory.get('/')
+        request.user = self.user
+        response = index(request)
+        self.assertEqual(response.status_code, 200)
