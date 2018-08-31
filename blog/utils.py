f937a54a4a5149ae501d130f7cafd480df3e2fa0
Pedro Miguel <pedro.miguel@live.co.uk>
2018-08-31 11:15:19 +0100

    Move pagination to app utils

--- blog/utils.py
+++ blog/utils.py
@@ -1,4 +1,22 @@
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.http import Http404
+
+
+def pagination(queryset, paginate_by, page_number):
+    # show 7 posts per page
+    paginator = Paginator(queryset, paginate_by)
+    # page = request.GET.get('page')
+
+    try:
+        page = paginator.page(page_number)
+    except PageNotAnInteger:
+        # if page is not an integer, deliver first page.
+        page = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range (e.g. 9999), deliver
+        # last page of results.
+        page = paginator.page(paginator.num_pages)
+    return (paginator, page, page.object_list, page.has_other_pages())
 
 
 class StaffUserMixin:
