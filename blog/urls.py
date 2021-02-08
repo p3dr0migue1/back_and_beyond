f9ab2243505067236b13bb7a25eeee0388f6ffcf
Pedro Miguel <pedro.miguel@live.co.uk>
2021-02-08 10:34:25 +0000

    Fix tests deprecated dependencies

--- blog/urls.py
+++ blog/urls.py
@@ -1,3 +1,4 @@
+from django.urls import include
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
 
@@ -9,18 +10,18 @@ from .views import (
     PostsInTag,
     TagCreate,
     TagCreatePopUp,
-    custom_login,
     # post_search,
 )
 
 app_name = "blog"
 
 urlpatterns = [
-    url(r"^$", PostList.as_view(), name="index"),
-
-    url(r"^login/$", custom_login, name="login"),
+    # authentication
+    url(r"^login/$", auth_views.LoginView.as_view(), name="login"),
     url(r"^logout/$", auth_views.logout_then_login, name="logout"),
 
+    # blog
+    url(r"^$", PostList.as_view(), name="index"),
     url(r"^new-post/$", PostCreate.as_view(), name="new-post"),
     url(r"^new-tag/$", TagCreate.as_view(), name="new-tag"),
     url(r"^new-tag/popup/$", TagCreatePopUp.as_view(), name="new-tag-popup"),
